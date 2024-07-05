9eaf2aad3dc5ed7df9d6797a6823d2d9626219cc
tscunanan <tscunanan@mymail.mapua.edu.ph>
2024-07-05 22:53:39 +0800

    iso

--- admin.php
+++ admin.php
@@ -1,14 +1,24 @@
+<!DOCTYPE html>
 <html lang="en">
 
-
 <head>
-<meta charset="utf-8">
-<meta content="width=device-width, initial-scale=1.0" name="viewport">
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-<title>DXC-RESOURCE HUB Admin</title>
+  <title>DXC-RESOURCE HUB Admin</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
 
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
 
-<link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <!-- Google Fonts -->
+  <link href="https://fonts.gstatic.com" rel="preconnect">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
   <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
   <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
   <link href="assets/vendor/quill/quill.snow.css" rel="stylesheet">
@@ -16,58 +26,123 @@
   <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
   <link href="assets/vendor/simple-datatables/style.css" rel="stylesheet">
 
-
-
-<link href="assets/css/style.css" rel="stylesheet">
-
-<!--
-<link rel="stylesheet" href="css/astyle.css">
-!-->
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
 
 </head>
 
 <body>
-<!-- HEADER START -->   
-<header id="header" class="header fixed-top d-flex align-items-center">
 
-<!-- PROFILE IMAGE ICON -->  
+  <!-- ======= Header ======= -->
+  <header id="header" class="header fixed-top d-flex align-items-center">
 
-    <li class="nav-item dropdown pe-3">
-        <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
-            <img src="images/hinata.jpg" alt="Profile" class="rounded-circle">
-            <span class="d-none d-md-block dropdown-toggle ps-2"> HINATA</span>
-        </a>
-    
-<!-- DROPDOWN -->               
-    <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
-        <li>
-            <a class="dropdown-item d-flex align-items-center" href="#">
-                <i class="bi bi-box-arrow-right"></i>
-                <span>Sign Out</span>
-            </a>
-        </li>    
+    <div class="d-flex align-items-center justify-content-between">
+        <nav class="header-nav ms-auto">
+        <ul class="d-flex align-items-center">
 
-    </ul>
-    </li>
-</header>
+            <li class="nav-item dropdown">
 
-<aside id="sidebar" class="sidebar">
-    <ul class="sidebar-nav" id="sidebar-nav">
-        <li class="nav-item">
-            <a class="nav link collapsed" href="admin.php">
-                <i class="bi bi-grid"></i>
-                <span>Components</span>
-            </a>
-        </li>
-    </ul>
-</aside>
+            <li class="nav-item dropdown pe-3">
 
+            <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
+                <img src="assets/img/profile-img.jpg" alt="Profile" class="rounded-circle">
+                <span class="d-none d-md-block dropdown-toggle ps-2">S. Hinata</span>
+            </a><!-- End Profile Iamge Icon -->
 
+            <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
+                <li class="dropdown-header">
+                <h6>Hinata Sohara</h6>
+                <span>Admin</span>
+                </li>
+                <li>
+                <hr class="dropdown-divider">
+                </li>
 
+                <li>
+                <a class="dropdown-item d-flex align-items-center" href="#">
+                    <i class="bi bi-box-arrow-right"></i>
+                    <span>Sign Out</span>
+                </a>
+                </li>
 
+            </ul><!-- End Profile Dropdown Items -->
+            </li><!-- End Profile Nav -->
 
+        </ul>
+        </nav><!-- End Icons Navigation -->
+    </div><!-- End Logo -->
+ 
+  </header><!-- End Header -->
 
+  
+  <main id="main" class="main">
 
+    <div class="pagetitle">
+      <h1>Dashboard</h1>
+      <nav>
+        <ol class="breadcrumb">
+          <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+          <li class="breadcrumb-item active">Dashboard</li>
+        </ol>
+      </nav>
+    </div><!-- End Page Title -->
+
+    <section class="section dashboard">
+      <div class="row">
+
+        <!-- Left side columns -->
+        <div class="col-lg-12">
+          <div class="row">
+
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">Add Resource</h5>
+
+              <!-- Horizontal Form -->
+              <form>
+                <div class="row mb-3">
+                  <label for="inputEmail3" class="col-sm-2 col-form-label">Title</label>
+                  <div class="col-sm-10">
+                    <input type="text" class="form-control" id="inputText">
+                  </div>
+                </div>
+                <div class="row mb-3">
+                  <label for="inputEmail3" class="col-sm-2 col-form-label">Content</label>
+                  <div class="col-sm-10">
+                    <input type="email" class="form-control" id="inputEmail">
+                  </div>
+                </div>
+                <div class="text-center">
+                  <button type="submit" class="btn btn-primary">Submit</button>
+                  <button type="reset" class="btn btn-secondary">Reset</button>
+                </div>
+              </form><!-- End Horizontal Form -->
+
+            </div>
+          </div>
+
+          </div>
+        </div><!-- End Left side columns -->
+
+      </div>
+    </section>
+
+  </main><!-- End #main -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/apexcharts/apexcharts.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/chart.js/chart.umd.js"></script>
+  <script src="assets/vendor/echarts/echarts.min.js"></script>
+  <script src="assets/vendor/quill/quill.min.js"></script>
+  <script src="assets/vendor/simple-datatables/simple-datatables.js"></script>
+  <script src="assets/vendor/tinymce/tinymce.min.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
 
 </body>
 
---
+++ oldAdmin.php
@@ -0,0 +1,74 @@
+<html lang="en">
+
+
+<head>
+<meta charset="utf-8">
+<meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+<title>DXC-RESOURCE HUB Admin</title>
+
+
+<link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/quill/quill.snow.css" rel="stylesheet">
+  <link href="assets/vendor/quill/quill.bubble.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/simple-datatables/style.css" rel="stylesheet">
+
+
+
+<link href="assets/css/style.css" rel="stylesheet">
+
+<!--
+<link rel="stylesheet" href="css/astyle.css">
+!-->
+
+</head>
+
+<body>
+<!-- HEADER START -->   
+<header id="header" class="header fixed-top d-flex align-items-center">
+
+<!-- PROFILE IMAGE ICON -->  
+
+    <li class="nav-item dropdown pe-3">
+        <a class="nav-link nav-profile d-flex align-items-center pe-0" href="#" data-bs-toggle="dropdown">
+            <img src="images/hinata.jpg" alt="Profile" class="rounded-circle">
+            <span class="d-none d-md-block dropdown-toggle ps-2"> HINATA</span>
+        </a>
+    
+<!-- DROPDOWN -->               
+    <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow profile">
+        <li>
+            <a class="dropdown-item d-flex align-items-center" href="#">
+                <i class="bi bi-box-arrow-right"></i>
+                <span>Sign Out</span>
+            </a>
+        </li>    
+
+    </ul>
+    </li>
+</header>
+
+<aside id="sidebar" class="sidebar">
+    <ul class="sidebar-nav" id="sidebar-nav">
+        <li class="nav-item">
+            <a class="nav link collapsed" href="admin.php">
+                <i class="bi bi-grid"></i>
+                <span>Components</span>
+            </a>
+        </li>
+    </ul>
+</aside>
+
+
+
+
+
+
+
+
+</body>
+
+</html>
